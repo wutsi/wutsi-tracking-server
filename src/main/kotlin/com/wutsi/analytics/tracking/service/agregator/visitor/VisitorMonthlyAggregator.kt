86d1c402d95bac76956629faae722bae24f0a5e0
wutsi <herve.tchepannou@wutsi.com>
2022-04-12 06:28:56 -0400

    remove visitor aggregators

--- src/main/kotlin/com/wutsi/analytics/tracking/service/agregator/visitor/VisitorMonthlyAggregator.kt
+++
@@ -1,5 +0,0 @@
-package com.wutsi.analytics.tracking.service.agregator.visitor
-
-import com.wutsi.analytics.tracking.service.agregator.visit.VisitMonthlyAggregator
-
-open class VisitorMonthlyAggregator : VisitMonthlyAggregator()
